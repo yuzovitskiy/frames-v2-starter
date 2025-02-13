708e5708ecbdb48aca285a92fe740db1032b9422
yuzovitskiy <yuzovitskiy@gmail.com>
2025-02-13 14:26:53 -0500

    simple meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,20 +26,10 @@ export const metadata: Metadata = {
     description: "A starter for Frame v2",
   },
   other: {
-    'fc:frame': JSON.stringify({
-      version: 'next',
-      imageUrl: `${baseUrl}/images/feed.png`,
-      button: {
-        title: 'Press Me',
-        action: {
-          type: 'launch_frame',
-          name: 'Frame V2 Starter',
-          url: baseUrl,
-          splashImageUrl: `${baseUrl}/images/splash.png`,
-          splashBackgroundColor: '#f7f7f7'
-        }
-      }
-    })
+    'fc:frame': 'vNext',
+    'fc:frame:image': `${baseUrl}/images/feed.png`,
+    'fc:frame:button:1': 'Press Me',
+    'fc:frame:post_url': `${baseUrl}/api/frame`,
   }
 };
 
